2de186abdc0111fd0494b6c1ae0da5b47fc00c1e
Minwoo <made_power@naver.com>
2023-08-26 18:32:04 +0900

    docker cp
    docker cp 명령어를 통해 컨테이너에 파일을 복사할 수 있다. 또한 컨테이너 내부의 파일을 로컬로 가져오는 것도  가능하다.

--- rng.py
+++ rng.py
@@ -11,3 +11,8 @@ else:
   print(rnd_number)
 
 print('image check -> docker image inspect')
+print('docker cp- 파일 복사 dummy에 있는 파일을 모두 복사한다.' )
+print('docker cp dummy/.  컨테이너_이름:/컨테이너_내부경로(:/test)' )
+print('docker cp 컨테이너_이름 로컬폴더')
+print('위 커멘드로 컨테이너 내부를 확인해볼 수 있다. 구체적인 파일을 입력해도 된다.')
+
