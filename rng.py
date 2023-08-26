b7a5ce6ac466766b7f6cf0561092a590a1c0456e
Minwoo <made_power@naver.com>
2023-08-26 18:41:46 +0900

    docker run -- name
    컨테이너의 이름을 지정하는 커맨드이다. 기존 랜덤으로 생성되는 컨테이너 이름을 원하는 방식으로 지정해 줄 수 있다.
    image의 경우 이름과 태그가 지정 가능하다.
    docker build -t goals:latest 를 사용하면 이름이 goals인 이미지가 최신 버전임을 나타낼 수 있다.

--- rng.py
+++ rng.py
@@ -16,3 +16,6 @@ print('docker cp dummy/.  컨테이너_이름:/컨테이너_내부경로(:/test)
 print('docker cp 컨테이너_이름 로컬폴더')
 print('위 커멘드로 컨테이너 내부를 확인해볼 수 있다. 구체적인 파일을 입력해도 된다.')
 
+
+print('docker run --name 을 통한 컨테이너 이름 지정')
+print('docker name:tag'    ,' 태그에는 버전이나 다른 구성이 있는경우 이름과 태그를 결합할 수 있다.')
